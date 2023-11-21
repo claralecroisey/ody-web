657cb98a1d156a3bd8e4242f37a9134fb0fbbcd1
Clara Lecroisey <clalecroisey@gmail.com>
2023-11-21 16:46:14 +0100

    chore: clean up

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,30 +1,7 @@
 import { useAuth0 } from '@auth0/auth0-react';
-import { useEffect } from 'react';
 
 export default function Home() {
-  const { getAccessTokenSilently, user } = useAuth0();
-
-  useEffect(() => {
-    async function fetchData() {
-      const token = await getAccessTokenSilently();
-      const config = {
-        method: 'GET',
-        headers: {
-          'content-type': 'application/json',
-          Authorization: `Bearer ${token}`,
-        },
-      };
-
-      const response = await fetch(
-        `${import.meta.env.VITE_API_BASE_URL}/test`,
-        config,
-      );
-      const data = await response.json();
-      console.log(data);
-    }
-
-    fetchData();
-  }, [getAccessTokenSilently]);
+  const { user } = useAuth0();
 
   return (
     <div className="container">
